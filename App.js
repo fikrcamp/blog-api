643f41f7d0eb946c25e455fd018b56b92f2d2260
aroble <abdulladif0042@gmail.com>
2022-12-30 17:23:39 +0300

    inital commit

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const expense = [{id:1,description:"Food",amount:35}]
+const express = require("express")
 
+const app = express()
 
+app.use(express.json())
+
+const authRoutes = require("./Routes/authRoutes")
+
+app.use("/auth",authRoutes)
+
+const PORT = 8000
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
---
+++ Controllers/authController.js
@@ -0,0 +1,10 @@
+
+exports.login = (req,res)=>{
+    res.status(200).json({message:"Hello from Login"})
+}
+
+exports.signup = (req,res)=>{
+    res.status(200).json({message:"Hello from Signup"})
+}
+
+
